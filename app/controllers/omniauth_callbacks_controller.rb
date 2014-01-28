5349a019aac9e671c0717908f40739355aaa0fb2
Bruce Steedman <bruce.steedman@gmail.com>
2014-01-28 17:50:33 +0000

    Configured for Heroku

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -1,9 +1,10 @@
 class OmniauthCallbacksController < ApplicationController
 
-  # method added by Devise :)
+  # method added by Devise :) called by twitter upon successful authentication
   def twitter
-
-    User.twitter_auth(request.env['omniauth.auth'])
+    user = User.twitter_auth(request.env['omniauth.auth']) # gets auth hash from Twitter
+    # tell Devise to sign user in and redirect
+    sign_in_and_redirect user
   end
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -8,19 +8,20 @@ class User < ActiveRecord::Base
 
   has_many :articles
 
-  def self.twitter_auth(auth_hashh)
+  def self.twitter_auth(auth_hash) # pass in Twitter auth hash
     # see gem omniauth-twitter docs for this:-
-    user = find_or_create_by(uid: auth[:uid], provider: :twitter) do |user|
-      user.name = auth[:info][:name]
-      user.password = pwd
-      user.password_confirmation = pwd
-      twitter_hash = request.env['omniauth.auth'] # gets auth hash from Twitter
+    user = find_or_create_by(uid: auth_hash[:uid], provider: :twitter) do |user|
+      user.name = auth_hash[:info][:name]
+      # dummy data for Devise so user can be signed in
+      user.password = nil
+      user.password_confirmation = nil
     end
   end
 
-  def email_required? # overides Warden method???????????????????
+  def email_required? # overides Devise method
     return false if provider == 'twitter'
-    super # CALLS WHAT????????????
+    super # superclass method which returns 'true'!
   end
 
 end # of class
+
